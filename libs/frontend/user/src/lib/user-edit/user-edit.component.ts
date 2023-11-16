085f36bc304bd183ef5926e894f90023afead1da
J.Seremak <janko2002@outlook.com>
2023-11-16 13:20:02 +0100

    updates

--- libs/frontend/user/src/lib/user-edit/user-edit.component.ts
+++ libs/frontend/user/src/lib/user-edit/user-edit.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { IUser } from '@client-side-project/shared/api';
 
 @Component({
   selector: 'client-side-project-user-edit',
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.css'],
 })
-export class UserEditComponent {}
+
+export class UserEditComponent {
+  user: IUser = {
+    name: '',
+    id: '',
+    email: '',
+    password: ''
+  }
+
+
+  // constructor {
+  //   private UserService: UserService,
+  //   private Route: ActivatedRoute,
+  //   private Rrouter: Router,
+  // }
+
+}
--- libs/frontend/user/src/lib/user.service.ts
+++ libs/frontend/user/src/lib/user.service.ts
@@ -1,8 +1,10 @@
 import { Observable, throwError } from 'rxjs';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { map, catchError, tap } from 'rxjs/operators';
-import { ApiResponse, IUser } from '@client-side-project/shared/api';
+import { ApiResponse, IUser, ICreateUser } from '@client-side-project/shared/api';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 
 /**
  * See https://angular.io/guide/http#requesting-data-from-a-server
@@ -12,6 +14,7 @@ export const httpOptions = {
     responseType: 'json',
 };
 
+
 /**
  *
  *
@@ -20,7 +23,29 @@ export const httpOptions = {
 export class UserService {
     endpoint = 'http://localhost:3000/api/user';
 
-    constructor(private readonly http: HttpClient) {}
+private users$ = new BehaviorSubject<IUser[]>([]);
+
+
+    constructor(private readonly http: HttpClient) {
+
+        const users: IUser[] =[
+            {
+                id: '0',
+              name: 'John Smith',
+              email: 'j.smith@mail.com',
+              password: 'Secret123'
+            },
+            {
+                id: '1',
+                name: 'Katie Smith',
+                email: 'k.smith@mail.com',
+                password: 'Secret123'
+            }
+        ];
+        
+        this.users$.next([...this.users$.value, ...users]);
+
+    }
 
     /**
      * Get all items.
@@ -28,18 +53,20 @@ export class UserService {
      * @options options - optional URL queryparam options
      */
     public list(options?: any): Observable<IUser[] | null> {
-        console.log(`list ${this.endpoint}`);
+        // console.log(`list ${this.endpoint}`);
 
-        return this.http
-            .get<ApiResponse<IUser[]>>(this.endpoint, {
-                ...options,
-                ...httpOptions,
-            })
-            .pipe(
-                map((response: any) => response.results as IUser[]),
-                tap(console.log),
-                catchError(this.handleError)
-            );
+        // return this.http
+        //     .get<ApiResponse<IUser[]>>(this.endpoint, {
+        //         ...options,
+        //         ...httpOptions,
+        //     })
+        //     .pipe(
+        //         map((response: any) => response.results as IUser[]),
+        //         tap(console.log),
+        //         catchError(this.handleError)
+        //     );
+
+        return this.users$;
     }
 
     /**
@@ -48,19 +75,64 @@ export class UserService {
      */
     public read(id: string | null, options?: any): Observable<IUser> {
         console.log(`read ${this.endpoint}/${id}`);
-        return this.http
-            .get<ApiResponse<IUser>>(`${this.endpoint}/${id}`,  {
-                ...options,
-                ...httpOptions,
-            })
-            .pipe(
-                tap(console.log),
-                map((response: any) => response.results as IUser),
-                catchError(this.handleError)
-            );
+        // return this.http
+        //     .get<ApiResponse<IUser>>(`${this.endpoint}/${id}`,  {
+        //         ...options,
+        //         ...httpOptions,
+        //     })
+        //     .pipe(
+        //         tap(console.log),
+        //         map((response: any) => response.results as IUser),
+        //         catchError(this.handleError)
+        //     );
+        return this.users$.pipe(
+            map((userList) => userList.find((user) => user.id === id)),
+        ) as Observable<IUser>;
     }
 
 
+
+    public createUser(user : IUser | null) : boolean{
+        if (user == null) {
+            return false;
+        }
+
+        //TODO: random ID geration
+
+        //random id of oplopend
+        user.id = "randomGetal"
+
+        this.users$.next([...this.users$.value, user]);
+        return true;
+    }
+
+
+    public updateUser(user : IUser | null) : void {
+        if (user == null) {
+            return;
+        }
+
+        //delete
+        this.removeUser(user);
+
+        //create
+        this.users$.next([...this.users$.value, user]);
+      
+    
+    }
+
+    public removeUser(user : IUser | null) : void {
+        if (user == null) {
+            return;
+        }
+
+        // //delete
+        // this.users$.forEach((user, index) => {
+        //     if (item === data) { roomArr.splice(index, 1); }
+        //   });
+    
+    
+    }
 
     /**
      * Handle errors.
