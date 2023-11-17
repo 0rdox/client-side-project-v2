19c10e3ed7977fae16f590e85edfde24d97f64d5
J.Seremak <janko2002@outlook.com>
2023-11-17 14:05:27 +0100

    Added Env

---
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -0,0 +1,6 @@
+import { IEnvironment } from "./environment.interface";
+
+export const environment: IEnvironment = {
+    production: true,
+    dataApiUrl: "https://avansnxworkshop.azurewebsites.net/api",
+}
