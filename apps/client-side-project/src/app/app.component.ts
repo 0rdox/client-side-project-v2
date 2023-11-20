4bb37318c436f3f9b29ee9f67d5dd86da62db416
J.Seremak <janko2002@outlook.com>
2023-11-20 19:09:27 +0100

    After Deletion Gallery

--- apps/client-side-project/src/app/app.component.ts
+++ apps/client-side-project/src/app/app.component.ts
@@ -4,11 +4,10 @@ import { FeaturesModule } from '@client-side-project/frontend/features'
 import { UiModule } from '@client-side-project/frontend/ui'
 import { NxWelcomeComponent } from './nx-welcome.component';
 
-import { GalleryModule } from '@client-side-project/frontend/gallery';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, FeaturesModule, UiModule, GalleryModule],
+  imports: [NxWelcomeComponent, RouterModule, FeaturesModule, UiModule],
   selector: 'client-side-project-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
